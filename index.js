09076f4734c2a72f8a5979fb1e52e9e108715198
Netaly Ramirez <21netaly@gmail.com>
2020-03-16 19:31:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function updateObjectWithKeyAndValue(obj,key,val){
- newObj = Object.assign({},obj, {[key] : val})
+return  Object.assign({},obj, {[key] : val})
  //  object.assign (new obj, obj you are copying, {new key value pair})
-  return newObj
+  
 }
